5a36eac2057c7eac5273d53742272f4526942957
sisa00005 <sisa00005@stud.uni-saarland.de>
2026-05-14 12:35:17 +0200

    v2.0

--- code/model.py
+++ code/model.py
@@ -1,8 +1,8 @@
+
 import os
 import torch
 import torch.nn as nn
 import torch.optim as optim
-
 from torchvision import datasets, transforms, models
 from torch.utils.data import DataLoader
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
